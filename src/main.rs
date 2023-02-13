cd0896376592cf42f7b7d78cb596e762f3737e38
ranafge@gmail.com <ranafge@gmail.com>
2023-02-14 02:41:47 +0600

    Guessing grame complete

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,38 @@
 
 // cargo new, takes the name of the project as the frist argument.
 
-use std::io; 
 use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is : {}", secret_number);
-    println!("Please input your guess..." );
-    // let used to create a variable. rust variables are immutable by default.
-    let mut guess = String::new(); // new instace of String
-    // process the user input
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Faild to read line");
 
-    println!("Your guessed number: {}", guess);
+    loop {
+        println!("Please input your guess...");
+        // let used to create a variable. rust variables are immutable by default.
+        let mut guess = String::new(); // new instace of String
+                                       // process the user input
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Faild to read line");
+
+        // Compare number
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Your guessed number: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
