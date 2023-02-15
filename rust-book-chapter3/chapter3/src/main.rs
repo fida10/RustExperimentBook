0a9979aaeecc6bf78ed192ff3cd273f3efc8f1d8
ranafge <ranafge@gmail.com>
2023-02-15 09:55:52 +0600

    declare a const variable

--- rust-book-chapter3/chapter3/src/main.rs
+++ rust-book-chapter3/chapter3/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    // variables and mutability
+    // constans are not allow to change
+    // conns
+    const three_hours_in_seconds = 60 * 60 * 3; 
+
+
 }
